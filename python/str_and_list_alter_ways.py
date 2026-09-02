051de1da70d1cfa57cc35d4cc0f543d927d98a5c
ZhangJiale <17879385920@163.com>
2026-09-02 21:06:22 +0800

    add 字符串数组和整数数组之间的转换、列表与字符串之间的转换

--- python/str_and_list_alter_ways.py
+++ python/str_and_list_alter_ways.py
@@ -4,7 +4,7 @@ list(s)        # ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']  拆成
 s.split()      # ['hello', 'world']                                       按空白拆成单词
 s.split('l')   # ['he', '', 'o wor', 'd']                                 按指定字符拆
 
-# 列表 → 字符串
+# 列表 → 字符串 但需要注意的是join函数只能处理字符串数组
 lst = ['hello', 'world']
 ' '.join(lst)   # 'hello world'   用空格拼
 '-'.join(lst)   # 'hello-world'   用连字符拼
