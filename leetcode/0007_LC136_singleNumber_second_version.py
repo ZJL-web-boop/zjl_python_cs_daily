3511eafdb8ff01f22cb89a757390724b949d72e2
ZhangJiale <17879385920@163.com>
2026-09-07 07:52:27 +0800

    add LC 136.singleNumber v1:时间与空间复杂度

--- leetcode/0007_LC136_singleNumber_second_version.py
+++ leetcode/0007_LC136_singleNumber_second_version.py
@@ -3,12 +3,12 @@
 然后分别计算列表中元素总和sum1以及集合中的元素总和sum2，二者相减从而得到出现两次的数字的总和sum3，
 再用集合中元素的总和sum2减去sum3，得到的便是那个只出现一次的数字。
 
-时间复杂度为O(n)（n为列表长度），空间复杂度为O(1)
+时间复杂度为O(n)（n为列表长度），空间复杂度为O(n)
 """
 
 class Solution(object):
     def singleNumber(self, nums):
-        nums_set = set(nums) # 把列表强制转换为集合（实现去重的操作）
+        nums_set = set(nums) # 把列表强制转换为集合（实现去重的操作），注意这里增加了空间复杂度
         sum1 = sum(nums) # 计算列表中元素总和sum1
         sum2 = sum(nums_set) # 计算集合中的元素总和sum2
         sum3 = sum1 - sum2 # 出现两次的数字的总和sum3
